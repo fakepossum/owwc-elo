d2d11725d79c38c1e57003eac098ea005ff9f147
fakepossum <finnian.breen@gmail.com>
2026-03-25 17:50:22 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ st.set_page_config(page_title="OWWC Elo Dashboard", page_icon="🏆", layout="wi
 
 # --- 2. CONSTANTS & MAPPINGS ---
 BASE_ELO = 1500
-K = 48
+K = 24
 REVERSION_FACTOR = 0.8
 HIATUS_REVERSION = 0.6
 
